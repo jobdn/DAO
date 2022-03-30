9f59659fb3dc1513d53cb9ef958bc329901b5f81
Danila Pisarev <jobdan@bk.ru>
2022-03-30 15:26:55 +0300

    Added needed tasks

---
+++ tasks/deposit.ts
@@ -0,0 +1,18 @@
+import { task } from "hardhat/config";
+import { config } from "../config";
+
+task("deposit", "Make deposit for voting to dao")
+  .addParam("amount", "Amount of token to vote")
+  .setAction(async (taskArgs, hre) => {
+    const { ethers } = hre;
+    const [signer] = await ethers.getSigners();
+    const { amount } = taskArgs;
+
+    const dao = await ethers.getContractAt("DAO", config.DAO);
+
+    await dao.deposit(amount);
+
+    console.log(
+      `User ${signer.address} sended ${amount} to dao ${dao.address}`
+    );
+  });
